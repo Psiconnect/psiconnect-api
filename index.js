4999b3a0e7fb97dd06f0584a0cf760b6e63e812a
ferchox920 <fernandoramones92@gmail.com>
2023-01-26 19:40:31 -0300

    routes&querys

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import sequelize from "./src/config/db.js";
 import dotenv from "dotenv";
 import httpServer from "./src/config/http.js";
 import socket from "./src/config/socket.io.js";
+import { mapUserTesting } from "./src/testing/user.js";
 
 dotenv.config();
 
 async function bootstrap() {
   await sequelize.sync({ force: true });
+  await mapUserTesting()
   socket(httpServer)
   httpServer.listen(process.env.PORT, () => {
     console.log(`Servidor escuchando en el puerto ${process.env.PORT}`);
