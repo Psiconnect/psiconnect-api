be03a9a87a8bb6211c9fc2a6cb516acd04144f0b
ferchox920 <fernandoramones92@gmail.com>
2023-01-31 17:42:28 -0300

    dto postRegister

--- src/DTO/professionalDTO/prefesionalPostRegisterDTO.js
+++ src/DTO/professionalDTO/prefesionalPostRegisterDTO.js
@@ -1,6 +1,7 @@
 import { descriptionDTOSchema, linkedinDTOSchema, skillDTOSchema } from "../typeDTO.js";
 
 import { Type } from "@sinclair/typebox";
+import Ajv from "ajv";
 
 const PostRegisterProfessionalDTOSchema = Type.Object(
   {
@@ -22,4 +23,27 @@ const ajv = new Ajv({ allErrors: true })
   .addKeyword("kind")
   .addKeyword("modifier");
 
-  ajv.addFormat('skills',/(Winter|Summer|Spring|Autumn)\b/)
+  ajv.addFormat('skills',/(AMABLE|EMPATICO|SIMPATICO|MOTIVADOR|ENERGICO|CONSERVADOR|LOGICO|PERSUASIVO|ORGANIZADO|ANALITICO|PROGRESISTA)\b/)
+
+  addErrors(ajv);
+
+const validateSchema = ajv.compile(PostRegisterProfessionalDTOSchema);
+
+const professionalPostRegisterDTO = (req, res, next) => {
+    try {
+      const isDTOValid = validateSchema(req.body);
+  
+      if (!isDTOValid)
+        return res.status(400).send({
+          errors: validateSchema.errors.map((error) => error.message),
+        });
+  
+      next();
+    } catch (error) {
+      return res.status(error.status).json(error.message);
+    }
+  };
+  
+  
+  export default professionalPostRegisterDTO;
+  
