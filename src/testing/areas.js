f392340c4b9c753c9649675abe93788e9acba946
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-01 13:15:54 -0300

    add image to area

--- src/testing/areas.js
+++ src/testing/areas.js
@@ -1,10 +1,31 @@
 import AREA from "../models/AREAS.js";
 
-const user = ["Depresion","Ansiedad", "Autoestima", "Familia", "Genero"];
+const Data = [
+  {
+  name:'Depresion',
+  image:"https://res.cloudinary.com/dcdywqotf/image/upload/v1675267920/areas/Depresion_gud0dp.svg"
+},
+  {
+  name:'Ansiedad',
+  image:"https://res.cloudinary.com/dcdywqotf/image/upload/v1675267920/areas/Ansiedad_eseo02.svg"
+},
+  {
+  name:'Autoestima',
+  image:"https://res.cloudinary.com/dcdywqotf/image/upload/v1675267920/areas/Autoestima_vtrhtn.svg"
+},
+  {
+  name:'Familiar',
+  image:"https://res.cloudinary.com/dcdywqotf/image/upload/v1675267920/areas/Terapia_Familiar_r7uudx.svg"
+},
+  {
+  name:'Genero',
+  image:"https://res.cloudinary.com/dcdywqotf/image/upload/v1675267920/areas/ComunidadLGBT_vloo89.svg"
+},
+];
 
 export async function mapAreaTesting() {
 
-  user.map(async (u) => {
+  Data.map(async (u) => {
     await AREA.create({area: u });
   });
 }
