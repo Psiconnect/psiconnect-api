8ca883b6761065d065932815431fec486c83c1b6
ferchox920 <fernandoramones92@gmail.com>
2023-02-03 15:08:32 -0300

    testSkills

--- src/testing/skills.js
+++ src/testing/skills.js
@@ -1,3 +1,56 @@
 //// estos son los datos que habia en el enum de skills del modelo professional:
 // "AMABLE","EMPATICO","SIMPATICO","MOTIVADOR","ENERGICO","CONSERVADOR",
 // "LOGICO","PERSUASIVO","ORGANIZADO","ADAPTABLE","ANALITICO","PROGRESISTA"
+
+const skills = [
+  {
+    skill: "AMABLE",
+  },
+  {
+    skill: "EMPATICO",
+  },
+  {
+    skill: "SIMPATICO",
+  },
+  {
+    skill: "MOTIVADOR",
+  },
+  {
+    skill: "SIMPATICO",
+  },
+  {
+    skill: "ENERGICO",
+  },
+  {
+    skill: "CONSERVADOR",
+  },
+  {
+    skill: "LOGICO",
+  },
+  {
+    skill: "PERSUASIVO",
+  },
+  {
+    skill: "ORGANIZADO",
+  },
+  {
+    skill: "ADAPTABLE",
+  },
+  {
+    skill: "ANALITICO",
+  },
+  {
+    skill: "PROGRESISTA",
+  },
+];
+
+
+
+
+export async function mapSkillsTesting() {
+
+    skills.map(async (u) => {
+     
+      await SKILLS.create({ ...u});
+    });
+}
