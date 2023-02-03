843c410ec5a30204884de1f07871b4eade83bcf6
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-03 05:18:55 -0300

    AAAA

--- src/routes/ares.routes.js
+++ src/routes/ares.routes.js
@@ -13,6 +13,16 @@ areaRoutes.get('/', async (req, res) => {
       return res.status(500).json({ data: error.message });
     }
   })
+areaRoutes.get('/professional/:Area', async (req, res) => {
+  const {Area} = req.params;
+    try {
+      const data = await findAllProfessionalWithArea(Area);
+      if (!data) return res.status(400).json("Base de datos vacia");
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({ data: error.message });
+    }
+  })
 
 
   
