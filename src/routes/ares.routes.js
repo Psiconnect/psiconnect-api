2f4b55e0de41811d9b55f53605c96dc4573cf579
ferchox920 <fernandoramones92@gmail.com>
2023-02-06 18:02:03 -0300

    commit

--- src/routes/ares.routes.js
+++ src/routes/ares.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { findAllArea, findAllProfessionalWithArea } from "../query/queryToArea.js";
+import { findAllArea, findAllProfessionalWithArea, findOnlyAreas } from "../query/queryToArea.js";
 
 
 const areaRoutes = Router();
@@ -25,6 +25,20 @@ areaRoutes.get('/professional/:Area', async (req, res) => {
       return res.status(500).json({ data: error.message });
     }
   })
+
+  areaRoutes.get('/onlyAreas', async (req, res) => {
+    
+   
+      try {
+        const data = await findOnlyAreas();
+  
+        if (!data) return res.status(400).json("Base de datos vacia");
+        return res.status(200).json(data);
+      } catch (error) {
+        return res.status(500).json({ data: error.message });
+      }
+    })
+  
 
 
   
