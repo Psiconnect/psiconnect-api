091d8ebecf0a361d4edce4dce9a43c1b1af1ae12
Kary Salas <karydelvalle@gmail.com>
2023-02-12 08:30:26 -0400

    'config cloudinary'

--- src/routes/cloudinary.routes.js
+++ src/routes/cloudinary.routes.js
@@ -1,17 +1,26 @@
 import { Router } from 'express'
+import cloudinary from 'cloudinary'
+import dotenv from 'dotenv'
 
 
 const cloudinaryRoutes = Router()
 
-cloudinaryRoutes.post('img/upload', async (req, res) => {
-    const { file } = req.file
-    try{
-        const response = await cloudinary.uploader.upload(file.tempFilePath)
-            res.status(200).json(response)
-    }catch(error){
-        res.status(400).send(error)
-    }
+dotenv.config()  //configuracion de cloudinary
 
-
-
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_APY_KEY,
+    api_secret: process.env.CLOUD_APY_SECRET
 })
+
+cloudinaryRoutes.post('img/upload', async (req, res) => {
+    const { image } = req.image
+    try{
+        await cloudinary.uploader.upload(image.tempFilePath, (error, response))
+            res.status(200).send({imageUrl : response.secure_url})
+    }catch(error){
+        res.status(500).send(error)
+    }
+})
+
+export default cloudinaryRoutes
