cc9637dc6570e4fce938d31640db86aa0a79b68c
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-15 02:49:37 -0300

    agrege algunas propiedades a los modelos de usuario de profesionales, tambien encontre un bug

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { getUserById } from "../query/queryToUser.js";
+
+const adminRoutes = Router();
+
+adminRoutes.put("/disable-user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await getUserById(id);
+    if (!user) return res.status(404).json("No se encontro el usuario");
+    user.status = !user.status;
+    return res.status(200).json("Estado de Usuario editado correctamente");
+  } catch (error) {
+    return res.status(500).json({ data: error.message });
+  }
+});
+
+export default adminRoutes;
