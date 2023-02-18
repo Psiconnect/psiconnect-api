e7d7ea92f0885509b902098b182686a1a3126a23
Kary Salas <karydelvalle@gmail.com>
2023-02-18 08:10:19 -0400

    modelo y rutas review

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -7,19 +7,26 @@ import { findAllReviews } from "../query/queryToReview.js";
 const reviewRoutes = Router();
 
 reviewRoutes.get("/:professionalId", async (req, res) => {
+  const {professionalId} = req.params
   try {
     const reviews = await findAllReviews();
-    const mapReviews = reviews.map((el) => {
-      return {
-        id: el.id,
-        score: el.score,
-        comments: el.comments,
-        userId: el.userId,
-        professionalId: el.professionalId,
-        username: el.user.name,
-        lastusername: el.user.lastName,
-      };
-    });
+    const filterReviews = reviews?.filter((el) => el.professionalId === professionalId );
+    const mapReviews = filterReviews?.map(el => {
+        return {
+          id : el.id,
+          score : el.score,
+          comments: el.comments,
+          userId: el.userId,
+          professionalId : el.professionalId,
+          puntualidad: el.puntualidad,
+          trato: el.trato,
+          general: el.general,
+          username: el.user.name,
+          lastusername: el.user.lastName,
+          professionalName : el.professional.name,
+          lastprofessionalName : el.professional.lastName
+        }
+    })    
     if (!reviews) return res.status(400).json("Base de datos vacia");
     return res.status(200).json(mapReviews);
   } catch (error) {
@@ -28,9 +35,8 @@ reviewRoutes.get("/:professionalId", async (req, res) => {
 });
 
 reviewRoutes.post("/:professionalId", async (req, res) => {
-  const { comments, score, userId } = req.body;
+  const { comments, score, userId, puntualidad, trato, general } = req.body;
   const { professionalId } = req.params;
-
   try {
     const professional = await getProfessionalById(professionalId);
     const users = await getUserById(userId);
@@ -38,6 +44,9 @@ reviewRoutes.post("/:professionalId", async (req, res) => {
       return res.status(404).json({ data: "datos no encontrados" });
     }
     const newReview = await REVIEW.create({
+      puntualidad,
+      trato,
+      general,
       comments,
       score,
       userId,
@@ -75,35 +84,8 @@ reviewRoutes.get("/", async (req, res) => {
   });
 
 
-reviewRoutes.post('/:professionalId', async (req, res ) => {
-    const { comments, score, userId, puntualidad, trato, general, } = req.body  
-    const { professionalId } = req.params
-
-    try{
-        const professional = await getProfessionalById(professionalId)
-        const users = await getUserById(userId)
-        if(!professional || !users) {
-            return res.status(404).json({data: 'datos no encontrados'})
-        }
-        const newReview = await REVIEW.create({
-            comments,
-            score,     
-            userId,
-            professionalId,
-            puntualidad,
-            trato,
-            general,
-        })
-
-        return res.status(200).json(newReview)
-    } 
-    catch (error) {
-        return res.status(500).json({ data: error.message })
-    }
 
 
-
-})
 
 
 
