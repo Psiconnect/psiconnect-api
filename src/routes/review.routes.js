6469f1bda440f7388020374a1422ea67d7c5e482
Kary Salas <karydelvalle@gmail.com>
2023-02-19 07:51:15 -0400

    routesreview se agrego ? al user

--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -21,10 +21,10 @@ reviewRoutes.get("/:professionalId", async (req, res) => {
           puntualidad: el.puntualidad,
           trato: el.trato,
           general: el.general,
-          username: el.user.name,
-          lastusername: el.user.lastName,
-          professionalName : el.professional.name,
-          lastprofessionalName : el.professional.lastName
+          username: el.user?.name,
+          lastusername: el.user?.lastName,
+          professionalName : el.professional?.name,
+          lastprofessionalName : el.professional?.lastName
         }
     })    
     if (!reviews) return res.status(400).json("Base de datos vacia");
@@ -39,6 +39,7 @@ reviewRoutes.post("/:professionalId", async (req, res) => {
   const { professionalId } = req.params;
   try {
     const professional = await getProfessionalById(professionalId);
+    console.log(professional, 'profesional')
     const users = await getUserById(userId);
     if (!professional || !users) {
       return res.status(404).json({ data: "datos no encontrados" });
@@ -71,12 +72,12 @@ reviewRoutes.get("/", async (req, res) => {
             puntualidad: el.puntualidad,
             trato: el.trato,
             general: el.general,
-            username: el.user.name,
-            lastusername: el.user.lastName
+            username: el.user?.name,
+            lastusername: el.user?.lastName
         }
     })
     if(!reviews) return  res.status(400).json("Base de datos vacia");
-    return res.status(200).json(mapReviews)
+    return res.status(200).json(reviews)
 
     }catch(error) {
         return res.status(404).json({data: error.message})
