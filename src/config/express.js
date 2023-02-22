73d8fe9816e29fb9d35483fa80f78a7b010eee90
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-22 13:54:53 -0300

    borrado logico arreglado de areas

--- src/config/express.js
+++ src/config/express.js
@@ -19,8 +19,8 @@ const expressApp = express();
 expressApp.use(express.json());
 expressApp.use(morgan("dev"));
 expressApp.use(cors());
-expressApp.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
-expressApp.use(bodyParser.json({ limit: "50mb" }));
+expressApp.use(bodyParser.urlencoded({limit: "500mb", extended: true, parameterLimit:500000}));
+expressApp.use(bodyParser.json({ limit: "500mb" }));
 expressApp.use(cookieParser());
 expressApp.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
