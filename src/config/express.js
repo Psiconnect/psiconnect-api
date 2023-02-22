03eaf07ae81272be61bcea6c1905442d3865c08f
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-22 14:07:44 -0300

    borrado logico arreglado de areas

--- src/config/express.js
+++ src/config/express.js
@@ -24,12 +24,9 @@ expressApp.use(bodyParser.json({ limit: "500mb" }));
 expressApp.use(cookieParser());
 expressApp.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', '*');
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
   next();
 });
 
