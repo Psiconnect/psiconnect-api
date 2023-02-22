baba0607bf147d6fa1ca763d320ff8181ee99cf5
ArceDaniel <jonathandanielarce9@gmail.com>
2023-02-22 13:58:10 -0300

    borrado logico arreglado de areas

--- src/routes/cloudinary.routes.js
+++ src/routes/cloudinary.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary'
 import dotenv from "dotenv";
 
 const cloudinaryRoutes = Router();
